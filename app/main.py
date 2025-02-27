f07243919e07b4f74d8c9671103b258a65174e99
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-27 20:02:10 +0800

    Fix import statement in main.py to reference routers directly

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import order_router  
+from routers import order_router  
 from . import models, database  
 
 models.Base.metadata.create_all(bind=database.engine)
