79b7ee0dcd281901ef1381a4581a67ffc350f566
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-27 20:31:38 +0800

    Update Dockerfile to enable auto-reload and fix import statements in main.py for absolute paths

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import order_router  
-from . import models, database  
+from app.routers import order_router  
+from app import models, database  
 
 models.Base.metadata.create_all(bind=database.engine)
 
